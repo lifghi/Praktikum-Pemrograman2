977b48e46afa11b355bc099a42c4155bf620e8a2
lifghi <115357938+lifghi@users.noreply.github.com>
2023-10-03 16:53:55 +0800

    Delete Modul1 directory

--- Modul1/PRAK104_2210817310014_FatihAlifGhifari.java
+++
@@ -1,9 +0,0 @@
-package Modul1;
-
-public class PRAK104_2210817310014_FatihAlifGhifari {
-	public static void main(String[] args) {
-        String nama ="Fatih Alif Ghifari";
-        int umur =19;
-        System.out.println("Perkenalkan, Nama Saya " + nama + ", Umur saya " + umur );
-    }
-}
